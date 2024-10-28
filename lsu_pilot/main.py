346237bc470b1055c10f31f9a1c37aabcdd9dd30
Nithish Raghunandanan <12782505+nithishr@users.noreply.github.com>
2024-10-28 20:51:45 +0100

    Implement code generation using function calling

--- lsu_pilot/main.py
+++ lsu_pilot/main.py
@@ -1,12 +1,20 @@
 import os
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
 from dotenv import load_dotenv
 from openai import OpenAI
 import pandas as pd
 import numpy as np
 from .questions import answer_question
+from .functions import functions, run_function
+import json
 
 # Get the directory of the current script
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -21,12 +29,48 @@ load_dotenv()  # take environment variables from .env.
 openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 tg_bot_token = os.getenv("TG_BOT_TOKEN")
 
+CODE_PROMPT = """
+Here are two input:output examples for code generation. Please use these and follow the styling for future requests that you think are pertinent to the request.
+Make sure All HTML is generated with the JSX flavoring.
+// SAMPLE 1
+// A Blue Box with 3 yellow cirles inside of it that have a red outline
+<div style={{   backgroundColor: 'blue',
+  padding: '20px',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  width: '300px',
+  height: '100px', }}>
+  <div style={{     backgroundColor: 'yellow',
+    borderRadius: '50%',
+    width: '50px',
+    height: '50px',
+    border: '2px solid red'
+  }}></div>
+  <div style={{     backgroundColor: 'yellow',
+    borderRadius: '50%',
+    width: '50px',
+    height: '50px',
+    border: '2px solid red'
+  }}></div>
+  <div style={{     backgroundColor: 'yellow',
+    borderRadius: '50%',
+    width: '50px',
+    height: '50px',
+    border: '2px solid red'
+  }}></div>
+</div>
+"""
 
 messages = [
     {
         "role": "system",
-        "content": "You are a helpful assistant that answers questions like a pirate.",
-    }
+        "content": "You are a helpful assistant that answers questions.",
+    },
+    {
+        "role": "system",
+        "content": CODE_PROMPT,
+    },
 ]
 
 logging.basicConfig(
@@ -42,16 +86,65 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     messages.append({"role": "user", "content": update.message.text})
-    completion = openai.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=messages,
+    initial_response = openai.chat.completions.create(
+        model="gpt-4o-mini", messages=messages, tools=functions
     )
+    initial_response_message = initial_response.choices[0].message
+    messages.append(initial_response_message)
+    final_response = None
+    tool_calls = initial_response_message.tool_calls
+    if tool_calls:
+        for tool_call in tool_calls:
+            name = tool_call.function.name
+            args = json.loads(tool_call.function.arguments)
+            response = run_function(name, args)
+            print(tool_calls)
+            if name == "svg_to_png_bytes":
+                await context.bot.send_photo(
+                    chat_id=update.effective_chat.id, photo=response
+                )
+                messages.append(
+                    {
+                        "tool_call_id": tool_call.id,
+                        "role": "tool",
+                        "name": name,
+                        "content": str(response)
+                        + "Image was sent to the user, do not send the base64 string to them. Only send back 'here is the svg rendered as requested'",
+                    }
+                )
+            else:
+                messages.append(
+                    {
+                        "tool_call_id": tool_call.id,
+                        "role": "tool",
+                        "name": name,
+                        "content": str(response),
+                    }
+                )
+            # Generate the final response
+            final_response = openai.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=messages,
+            )
+            final_answer = final_response.choices[0].message
 
-    answer = completion.choices[0].message
-    messages.append(answer)
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id, text=answer.content
-    )
+            # Send the final response if it exists
+            if final_answer:
+                messages.append(final_answer)
+                await context.bot.send_message(
+                    chat_id=update.effective_chat.id, text=final_answer.content
+                )
+            else:
+                # Send an error message if something went wrong
+                await context.bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    text="something wrong happened, please try again",
+                )
+    # no functions were execute
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, text=initial_response_message.content
+        )
 
 
 async def mozilla(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -63,7 +156,7 @@ if __name__ == "__main__":
     application = ApplicationBuilder().token(tg_bot_token).build()
 
     start_handler = CommandHandler("start", start)
-    chat_handler = CommandHandler("chat", chat)
+    chat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), chat)
     mozilla_handler = CommandHandler("mozilla", mozilla)
 
     application.add_handler(start_handler)
